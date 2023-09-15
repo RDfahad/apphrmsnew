914b234c4dcb258bec650cb90bf985052ec9609a
Fahad <fahad@therocketdigital.com>
2023-09-15 16:09:39 +0500

    refactoring, error fixing

--- lib/ui/screens/authentication/view/login_screen.dart
+++ lib/ui/screens/authentication/view/login_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hr_emp_proj/ui/screens/bottom_navigation/screen/bottom_navigation_screen.dart';
 import '/ui/screens/authentication/bloc/authentication_bloc.dart';
 import '/ui/screens/authentication/bloc/authentication_state.dart';
-import '/ui/screens/dashboard/view/dashboard_screen.dart';
 import '/ui/widgets/custom_text_field.dart';
 import '/ui/widgets/loader_widget.dart';
 import '/ui/widgets/round_button.dart';
@@ -23,8 +23,9 @@ class LoginScreen extends StatelessWidget {
       body: BlocConsumer<AuthenticationCubit, AuthenticationState>(
         listener: (context, state) {
           if (state.loginSuccessfull) {
+            print('done');
             Navigator.push(
-                context, CupertinoPageRoute(builder: (_) => DashBoardScreen()));
+                context, CupertinoPageRoute(builder: (_) => BottomNavigationScreen()));
           }
         },
         builder: (context, state) {
@@ -75,7 +76,7 @@ class LoginScreen extends StatelessWidget {
                         child: Column(
                           children: [
                             CustomTextFormField(
-                              preffixIcon: Icons.email,
+                              prefixIcon: Icons.email,
                               isIconColorChange: state.isIconFieldColorEnabled,
                               controller: state.emailController,
                               labelText: "Email",
@@ -105,77 +106,65 @@ class LoginScreen extends StatelessWidget {
                               ),
                             ),
                             // Expanded(child: SizedBox()),
-                            BlocBuilder<AuthenticationCubit,
-                                AuthenticationState>(
-                              builder: (context, state) {
-                                if (state.errorMessage != null) {
-                                  return Center(
-                                    child: Column(
-                                      children: [
-                                        const Icon(
-                                          Icons.warning_amber,
-                                          size: 30,
-                                          color: AppColor.redColor,
-                                        ),
-                                        SizedBox(
-                                            height:
-                                                context.getScreenHeight * 0.01),
-                                        Center(
-                                          child: Text(
-                                            state.errorMessage.toString(),
-                                            //"There are something wrong with your login\n credentials, please double check and try again.",
-                                            textAlign: TextAlign.center,
-                                            style: const TextStyle(
-                                                fontSize: 16,
-                                                color: AppColor.redColor,
-                                                fontWeight: FontWeight.w400),
-                                          ),
-                                        ),
-                                      ],
+                            state.error ? Expanded(
+                              child: Center(
+                                child: Column(
+                                  children: [
+                                    const Icon(
+                                      Icons.warning_amber,
+                                      size: 30,
+                                      color: AppColor.redColor,
                                     ),
-                                  );
-                                } else {
-                                  return Container();
-                                }
-                              },
-                            ),
+                                    SizedBox(
+                                        height:
+                                        context.getScreenHeight * 0.01),
+                                    Center(
+                                      child: Text(
+                                        state.errorMessage.toString(),
+                                        //"There are something wrong with your login\n credentials, please double check and try again.",
+                                        textAlign: TextAlign.center,
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            color: AppColor.redColor,
+                                            fontWeight: FontWeight.w400),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ) : Container(),
                             SizedBox(height: context.getScreenHeight * 0.04),
                           ],
                         ),
                       )),
                   Flexible(
                     flex: 1,
-                    child:
-                        BlocBuilder<AuthenticationCubit, AuthenticationState>(
-                      builder: (context, state) {
-                        return Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: context.getScreenWidth * 0.04),
-                          child: RoundElevatedButton(
-                            title: "Sign In",
-                            onPress: state.isButtonEnabled
-                                ? () {
-                                    context
-                                        .read<AuthenticationCubit>()
-                                        .loginUser(
-                                          email: state.emailController.text,
-                                          password:
-                                              state.passwordController.text,
-                                        );
-                                  }
-                                : null,
-                            width: double.infinity,
-                            borderRadius: 25,
-                            height: 10,
-                            buttonColor: (state.isButtonEnabled)
-                                ? AppColor.primaryButtonColor
-                                : AppColor.secondaryButtonColor,
-                            textColor: (state.isButtonEnabled)
-                                ? AppColor.whiteColor
-                                : AppColor.secondaryTextColor,
-                          ),
-                        );
-                      },
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: context.getScreenWidth * 0.04),
+                      child: RoundElevatedButton(
+                        title: "Sign In",
+                        onPress: state.isButtonEnabled
+                            ? () {
+                          context
+                              .read<AuthenticationCubit>()
+                              .loginUser(
+                            email: state.emailController.text,
+                            password:
+                            state.passwordController.text,
+                          );
+                        }
+                            : null,
+                        width: double.infinity,
+                        borderRadius: 25,
+                        height: context.getScreenHeight * 0.06,
+                        buttonColor: (state.isButtonEnabled)
+                            ? AppColor.primaryButtonColor
+                            : AppColor.secondaryButtonColor,
+                        textColor: (state.isButtonEnabled)
+                            ? AppColor.whiteColor
+                            : AppColor.secondaryTextColor,
+                      ),
                     ),
                   )
                 ],
